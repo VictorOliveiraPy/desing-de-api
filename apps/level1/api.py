bc4e86f79684e8ed44c2c25fd60bfa06b0c2df46
Victor Hugo Nascimento Oliveira <victor.oliveira@olist.com>
2021-09-25 10:54:45 -0300

    create decorators view

--- apps/level1/api.py
+++ apps/level1/api.py
@@ -1,72 +1,29 @@
-from http import HTTPStatus
-
 from coopy.base import init_persistent_system
-from django.http import HttpResponse
-from django.utils.datastructures import MultiValueDictKeyError
 
-from apps.level1.domain import Order, CoffeeShop, DoesNotExist
+from apps.level1.domain import CoffeeShop, DoesNotExist, Order
+from apps.level1.framework import Created, NoContent, NotFound, allow, require
 
 coffeeshop = init_persistent_system(CoffeeShop(), basedir='level1')
 
 
-def create(request):
-    if request.method != 'POST':
-        return HttpResponse(
-            HTTPStatus.METHOD_NOT_ALLOWED.description,
-            status=HTTPStatus.METHOD_NOT_ALLOWED,
-            headers={'Content-Type': 'text/plain; charset=utf-8'}
-        )
-    try:
-        params = {
-            k: request.GET[k]
-            for k in (
-                'coffe', 'size', 'milk', 'location'
-            )
-        }
-    except MultiValueDictKeyError as e:
-        return HttpResponse(
-            str(e).strip("'"),
-            status=HTTPStatus.BAD_REQUEST,
-            headers={'Content-Type': 'text/plain; charset=utf-8'}
-        )
-
+@allow(['POST'])
+@require('coffe', 'size', 'milk', 'location')
+def create(request, params):
     order = Order(**params)
     coffeeshop.place_order(order)
 
     body = f'Order={order.id}'
 
-    return HttpResponse(
-        body,
-        status=HTTPStatus.CREATED,
-        headers={'Content-Type': 'text/plain; charset=utf8'}
-    )
+    return Created()
 
 
-def delete(request):
-    if request.method != 'POST':
-        return HttpResponse(
-            HTTPStatus.METHOD_NOT_ALLOWED.description,
-            status=HTTPStatus.METHOD_NOT_ALLOWED,
-            headers={'Content-Type': 'text/plain; charset=utf-8'}
-        )
-    try:
-        params = {
-            k: request.GET[k]
-            for k in ('id',)
-        }
-    except MultiValueDictKeyError as e:
-        return HttpResponse(
-            str(e).strip("'"),
-            status=HTTPStatus.BAD_REQUEST,
-            headers={'Content-Type': 'text/plain; charset=utf-8'}
-        )
+@allow(['POST'])
+@require('id')
+def delete(request, params):
     try:
         order = Order(**params)
         coffeeshop.delete(order)
     except DoesNotExist as e:
-        return HttpResponse(
-            status=HTTPStatus.NOT_FOUND,
-            headers={'Content-Type': 'text/plain; charset=utf-8'}
-        )
+        return NotFound()
 
-    return HttpResponse(status=HTTPStatus.NO_CONTENT, headers={'Content-Type': 'text/plain; charset=utf-8'})
+    return NoContent()
